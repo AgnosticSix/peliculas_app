440c8eb15d96f19af7644fa3fa392b9e2a2425dc
AgnosticSix <mateo_godfather@hotmail.com>
2022-07-08 20:11:37 -0500

    card swiper added and a slider for movies in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Peliculas en cines'),
-        elevation: 0,
-      ),
-      body: Container(
-        child: const Center(
-          child: Text('Home Screen'),
+        appBar: AppBar(
+          title: const Text('Peliculas en cines'),
+          elevation: 0,
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search_outlined)),
+          ],
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              //Tarjetas principales
+              CardSwiper(),
+
+              //Slider de peliculas
+              MovieSlider(),
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/widgets/movie_slider.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MovieSlider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 250,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              'Populares',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 20,
+                itemBuilder: (_, int index) => _MoviePoster()),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MoviePoster extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      height: 190,
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: const [
+          FadeInImage(
+            placeholder: AssetImage('assets/loading.gif'),
+            image: NetworkImage('https://via.placeholder.com/300x400'),
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+  }
+}
